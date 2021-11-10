39d616f3d5ae6ffbf47878bbb31eadb7ba072ef2
Tom Wilson <wilsonte@umich.edu>
2021-11-09 21:49:09 -0500

    fix stage 1 flag

--- R/stage1.R
+++ R/stage1.R
@@ -3,8 +3,11 @@
 #---------------------------------------------------------------------------
 initializeJobManager <- function(mdiPath, developer = FALSE) {
     if(!developer) message('initializing the Stage 1 pipelines job manager')
-    args <- if(developer) c(mdiPath, 'develop', 'initialize')
+    args <- if(developer) c(mdiPath, '--develop', 'initialize')
                      else c(mdiPath,            'initialize')
+
+    # NOTE: this is NOT necessarily sufficient to load all program targets into mdi
+    # must run 'mdi initialize' from a shell on some systems
     tryCatch(
         { system2('bash', args = args) }, 
         warning = function(w) print(w),
