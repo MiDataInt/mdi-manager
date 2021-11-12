952c233a14e67643b08c781f76a70ee8d3fe9988
Thomas E Wilson <wilsonte@umich.edu>
2021-11-12 12:13:35 -0500

    confirm remote install.bat

--- inst/mdi-remote-install.bat
+++ inst/mdi-remote-install.bat
@@ -23,11 +23,35 @@ SET ADD_TO_PATH=TRUE
 REM do not edit anything below this line
 
 
+REM prompt for permission
+SET IP_MESSAGE=-(action suppressed)
+IF %INSTALL_PACKAGES%==TRUE (
+    SET IP_MESSAGE=- install or update R packages
+)
+SET ATP_MESSAGE=-(action suppressed)
+IF %ADD_TO_PATH%==TRUE (
+    SET ATP_MESSAGE=- modify '~/.bashrc' to add the mdi executable to PATH
+)
+ECHO.
+ECHO ------------------------------------------------------------------
+ECHO CONFIRM MDI INSTALLATION ACTIONS
+ECHO ------------------------------------------------------------------
+ECHO.
+ECHO   - populate %SERVER% directory %MDI_DIR% or %MDI_DIR%/mdi (as appropriate)
+ECHO   - clone or update MDI repositories from GitHub
+ECHO   - check out the most recent version of all definitive MDI repositories
+ECHO   %IP_MESSAGE%
+ECHO   %ATP_MESSAGE%
+ECHO.
+SET /p CONFIRMATION=Do you wish to continue? (type 'y' for 'yes'):
+
+IF "%CONFIRMATION%"=="y" (
+
 REM ssh into server and execute the install command sequence
 ssh %USER%@%SERVER% ^
 %R_LOAD%; ^
 Rscript -e """install.packages('remotes', repos = 'https://cloud.r-project.org')"""; ^
-Rscript -e """gC <- '~/gitCredentials.R'; if(file.exists(gC)) {source(gC); do.call(Sys.setenv, gitCredentials)} remotes::install_github('MiDataInt/mdi-manager')"""; ^
+Rscript -e """gC <- '~/gitCredentials.R'; if(file.exists(gC)) {source(gC); do.call(Sys.setenv, gitCredentials)}; remotes::install_github('MiDataInt/mdi-manager')"""; ^
 Rscript -e """mdi::install('%MDI_DIR%', installPackages = %INSTALL_PACKAGES%, addToPATH = %ADD_TO_PATH%)"""; ^
 echo; ^
 echo "Done"
@@ -35,3 +59,5 @@ echo "Done"
 REM 
 
 PAUSE
+
+)
